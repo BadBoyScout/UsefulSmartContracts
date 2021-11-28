4518add6277fd5bb532e2345532d724669e765b4
xzt14-zealot <quaczs@outlook.com>
2021-11-27 23:51:35 -0500

    Add files via upload

---
+++ ERC20/ShovelCoin.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.0 (token/ERC20/presets/ERC20PresetMinterPauser.sol)
+
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
